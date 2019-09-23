902b77355b0214bd6a29ce8b78eed30ff5b8e5f7
spooms11 <804912969@qq.com>
2019-09-23 19:56:02 +0800

    Add files via upload

---
+++ task.py
@@ -0,0 +1,65 @@
+import numpy as np
+from physics_sim import PhysicsSim
+
+class Task():
+    """Task (environment) that defines the goal and provides feedback to the agent."""
+    def __init__(self, init_pose=None, init_velocities=None, 
+        init_angle_velocities=None, runtime=5., target_pos=None):
+        """Initialize a Task object.
+        Params
+        ======
+            init_pose: initial position of the quadcopter in (x,y,z) dimensions and the Euler angles
+            init_velocities: initial velocity of the quadcopter in (x,y,z) dimensions
+            init_angle_velocities: initial radians/second for each of the three Euler angles
+            runtime: time limit for each episode
+            target_pos: target/goal (x,y,z) position for the agent
+        """
+        # Simulation
+        self.sim = PhysicsSim(init_pose, init_velocities, init_angle_velocities, runtime) 
+        self.action_repeat = 3
+
+        self.state_size = self.action_repeat * 6
+        self.action_low = 300
+        self.action_high = 900
+        self.action_size = 4
+
+        # Goal
+        self.target_pos = target_pos if target_pos is not None else np.array([0., 0., 0.]) 
+
+    def get_reward(self):
+        """Uses current pose of sim to return reward."""
+        '''
+        reward = 40 - 1.3*(abs(self.sim.pose[:3] - self.target_pos)).sum() - .1*abs(self.sim.linear_accel[2]) + 2.5*self.sim.v[2] \
+                 - .5*abs(self.sim.linear_accel[1]) - .5*abs(self.sim.v[1]) - .5*abs(self.sim.linear_accel[0]) - .5*abs(self.sim.v[0]) \
+                 - 0.25*abs(self.sim.angular_v).sum() - 0.25*abs(self.sim.angular_accels).sum()\
+                 #+ 0.5*self.sim.pose[2]
+        reward = .5*reward##13/3
+        '''
+        reward = 40 - 2.5*(abs(self.sim.pose[:3] - self.target_pos)).sum() - .05*abs(self.sim.linear_accel[2]) + 1.0*self.sim.v[2] \
+                 - .5*abs(self.sim.linear_accel[1]) - .5*abs(self.sim.v[1]) - .5*abs(self.sim.linear_accel[0]) - .5*abs(self.sim.v[0]) \
+                 - 0.25*abs(self.sim.angular_v).sum() - 0.25*abs(self.sim.angular_accels).sum()\
+                 #+ 0.5*self.sim.pose[2]
+        if abs(self.sim.pose[:3] - self.target_pos).sum() < 0.1:
+            reward += 40
+        #reward = .5*reward##13/3
+        #reward = -min(abs(self.target_pos[2] - self.sim.pose[2]), 20.0)
+        return reward
+
+    def step(self, rotor_speeds):
+        """Uses action to obtain next state, reward, done."""
+        reward = 0
+        pose_all = []
+        for _ in range(self.action_repeat):
+            done = self.sim.next_timestep(rotor_speeds) # update the sim pose and velocities 
+            reward += self.get_reward() 
+            if done:
+                reward -= 20
+            pose_all.append(self.sim.pose)
+        next_state = np.concatenate(pose_all)
+        return next_state, reward, done
+
+    def reset(self):
+        """Reset the sim to start a new episode."""
+        self.sim.reset()
+        state = np.concatenate([self.sim.pose] * self.action_repeat) 
+        return state
